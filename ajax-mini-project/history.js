8cd0efd926eab83cd50268ad7d9ddc765cbe297f
Erez <erezur5@gmail.com>
2024-05-27 17:28:45 +0300

    css styling

--- ajax-mini-project/history.js
+++ ajax-mini-project/history.js
@@ -1,15 +1,82 @@
+const displayBookImages = (books) => {
+    // Clear existing content
+    booksContainer.innerHTML = '';
 
-function updateHistory(book_name,operation,time,bookId) {
-    const historyDiv = document.getElementById('history-container');
-    if (historyDiv) {
-        console.log('history-container found');
-        const newP = document.createElement('p');
-        newP.classList.add('history-content');
-        newP.setAttribute('id', 'history-content');
-        newP.textContent = `${book_name}${operation}${time}${bookId}`; // Adding some text to see the new element
-        historyDiv.appendChild(newP);
-        console.log('New paragraph added');
-    } else {
-        console.error('Element with id "history-container" not found.');
-    }
-}
+    let rowDiv;
+    books.forEach((book, index) => {
+        // Start a new row after every 4 books
+        if (index % 4 === 0) {
+            rowDiv = document.createElement('div');
+            rowDiv.classList.add('row');
+
+            booksContainer.appendChild(rowDiv);
+        }
+
+        // Create book card div
+        const bookDiv = document.createElement('div');
+        bookDiv.classList.add('book-card');
+
+        // Create image element
+        const img = document.createElement('img');
+        img.src = book.image;
+        img.alt = book.book_name;
+
+        // Set the width and height of the image
+        img.width = 131.59; // Adjust this value as needed
+        img.height = 199.25; // Adjust this value as needed
+
+        // Append image to book card div
+        bookDiv.appendChild(img);
+
+        // Add event listener to book card div
+        bookDiv.addEventListener('click', () => {
+            // Populate modal with book details
+            modalTitle.textContent = `Book Name: ${book.book_name}`;
+            modalImage.src = book.image;
+            modalImage.alt = book.book_name;
+            modalAuthor.textContent = `Author: ${book.authors_name}`;
+            modalDescription.textContent = `Description: ${book.short_description}`;
+            modalPageNumber.textContent = `Page Number: ${book.num_pages}`;
+            modalCopyNumber.textContent = `Copies: ${book.num_copies}`;
+            modalCategory.textContent = `Category: ${book.categories}`;
+            modalISBN.textContent = `ISBN: ${book.ISBN}`;
+
+            // Display the modal
+            bookModal.style.display = 'block';
+            document.body.style.overflow = 'hidden';
+            // catching the buttons div
+
+            //delete
+            deleteButton.textContent = 'Delete';
+            deleteButton.addEventListener('click', (event) => {
+                deleteBook(book.id);
+            });
+        
+            buttonsDiv.appendChild(deleteButton);
+
+            //update
+            editButton.textContent = 'Edit';
+            editButton.addEventListener('click', (event) => {
+                updateModal.style.display = 'block';
+
+                // Populate update modal with current book data
+                document.getElementById('updateBookName').value = book.book_name;
+                document.getElementById('updateAuthorsName').value = book.authors_name[0];
+                document.getElementById('updateNumPages').value = book.num_pages;
+                document.getElementById('updateShortDescription').value = book.short_description;
+                document.getElementById('updateImage').value = book.image;
+                document.getElementById('updateNumCopies').value = book.num_copies;
+                document.getElementById('updateCategories').value = book.categories[0];
+                document.getElementById('updateISBN').value = book.ISBN;
+
+                updateButton.addEventListener('click', (event) => {
+                    updateBook(book.id);
+                });
+            });
+            buttonsDiv.appendChild(editButton);
+        });
+
+        // Append book card div to current row
+        rowDiv.appendChild(bookDiv);
+    });
+};
