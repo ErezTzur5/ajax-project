5380228dfd541219103bfa8613878ddda85d5b90
Erez <erezur5@gmail.com>
2024-05-28 15:23:26 +0300

    added css styling

--- ajax-mini-project/app.js
+++ ajax-mini-project/app.js
@@ -67,15 +67,15 @@ const displayBookImages = (books) => {
         // Add event listener to book card div
         bookDiv.addEventListener('click', () => {
             // Populate modal with book details
-            modalTitle.textContent = `Book Name: ${book.book_name}`;
+            modalTitle.innerHTML = `Book Name: ${book.book_name}`;
             modalImage.src = book.image;
             modalImage.alt = book.book_name;
-            modalAuthor.textContent = `Author: ${book.authors_name}`;
-            modalDescription.textContent = `Description: ${book.short_description}`;
-            modalPageNumber.textContent = `Page Number: ${book.num_pages}`;
-            modalCopyNumber.textContent = `Copies: ${book.num_copies}`;
-            modalCategory.textContent = `Category: ${book.categories}`;
-            modalISBN.textContent = `ISBN: ${book.ISBN}`;
+            modalAuthor.innerHTML = `<strong>Author:</strong> ${book.authors_name}`;
+            modalDescription.innerHTML = `<strong>Description:</strong> ${book.short_description}`;
+            modalPageNumber.innerHTML = `<strong>Page Number:</strong> ${book.num_pages}`;
+            modalCopyNumber.innerHTML = `<strong>Copies:</strong> ${book.num_copies}`;
+            modalCategory.innerHTML = `<strong>Category:</strong> ${book.categories}`;
+            modalISBN.innerHTML = `<strong>ISBN:</strong> ${book.ISBN}`;
 
             // Display the modal
             bookModal.style.display = 'block';
@@ -258,7 +258,6 @@ function hideLoader() {
 
 
 function nextHandler() {
-    
     if (bookPerPage === booksNumInPage) {
         pageNum++; // Increment the page number
         fetchBooks(pageNum);
